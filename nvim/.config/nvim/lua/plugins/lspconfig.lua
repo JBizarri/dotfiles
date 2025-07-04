71e82fc43d8450983bc2335207ebcc5b2cf50fc9
Jefferson Bizarri <jefferson.bizarri@gmail.com>
2025-07-04 13:04:32 -0300

    Add sh lsp, formatter and linter

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -263,6 +263,9 @@ return {
             },
           },
         },
+        bashls = {},
+        shfmt = {},
+        shellcheck = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
