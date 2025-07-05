ef84140779c4ba8a76835559dabacd08e393b0d5
Jefferson Bizarri <jefferson.bizarri@gmail.com>
2025-07-05 16:29:15 -0300

    Swap border i3 border colors and dunst adjustments

---
+++ dunst/.config/dunst/examples.sh
@@ -0,0 +1,3 @@
+notify-send -u low "Low Urgency Notification" "This is the body of the notification"
+notify-send -u normal "Normal Urgency Notification" "This is the body of the notification"
+notify-send -u critical "Critical Urgency Notification" "This is the body of the notification"
