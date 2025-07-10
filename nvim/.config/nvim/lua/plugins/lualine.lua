0e4ad26d8fe046cf53c32cdc01268a0a98c39d2e
Jefferson Bizarri <jefferson.bizarri@gmail.com>
2025-07-10 19:24:49 -0300

    Relative path on lualine and only necessary snippets

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -2,6 +2,6 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('lualine').setup { theme = 'dracula' }
+    require('lualine').setup { theme = 'dracula', sections = { lualine_c = { { 'filename', path = 1 } } } }
   end,
 }
