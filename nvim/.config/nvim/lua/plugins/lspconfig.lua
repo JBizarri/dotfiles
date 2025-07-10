e0f8bbb576e7b681d42ec7c0136020fd8e270e55
Jefferson Bizarri <jefferson.bizarri@gmail.com>
2025-07-10 19:59:30 -0300

    Format json with prettier

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -266,7 +266,7 @@ return {
         bashls = {},
         shfmt = {},
         shellcheck = {},
-        jsonls = {},
+        jsonls = { init_options = { provideFormatter = false } },
         prettier = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
